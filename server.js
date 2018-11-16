b51718b17ecbe7fb9c1edcf5652e4294fb179d88
ugwumsinachi nnadi <nkennannadi@gmail.com>
2018-11-16 02:36:55 +0100

    first commit

--- server.js
+++ server.js
@@ -46,8 +46,8 @@ store.on('error', function(error) {
 
 
 
-const port = 3000;
-//const port = process.env.PORT || 8000;
+//const port = 3000;
+const port = process.env.PORT || 8000;
 
 app.use(function(req, res, next)
 {
